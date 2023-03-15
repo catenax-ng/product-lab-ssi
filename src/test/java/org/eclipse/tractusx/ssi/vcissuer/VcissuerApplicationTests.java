8d130edeb249324b535e6db5ba0d46feeb7fbcc5
Dominik Pinsel <dominik.pinsel@mercedes-benz.com>
2023-03-15 11:49:46 +0100

    initial commit
    Signed-off-by: Dominik Pinsel <dominik.pinsel@mercedes-benz.com>

---
+++ src/test/java/org/eclipse/tractusx/ssi/vcissuer/VcissuerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eclipse.tractusx.ssi.vcissuer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcissuerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

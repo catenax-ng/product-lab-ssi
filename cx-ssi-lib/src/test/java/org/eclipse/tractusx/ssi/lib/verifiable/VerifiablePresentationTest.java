48be6065df2669ea487cc86c0a7a987068f8cfcf
Enrico Risa <enrico.risa@gmail.com>
2023-06-22 10:08:15 +0200

    chore: explain issue

--- cx-ssi-lib/src/test/java/org/eclipse/tractusx/ssi/lib/verifiable/VerifiablePresentationTest.java
+++ cx-ssi-lib/src/test/java/org/eclipse/tractusx/ssi/lib/verifiable/VerifiablePresentationTest.java
@@ -1,9 +1,12 @@
 package org.eclipse.tractusx.ssi.lib.verifiable;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URI;
 import java.util.Map;
 import lombok.SneakyThrows;
+import org.eclipse.tractusx.ssi.lib.model.verifiable.credential.VerifiableCredential;
 import org.eclipse.tractusx.ssi.lib.model.verifiable.presentation.VerifiablePresentation;
+import org.eclipse.tractusx.ssi.lib.serialization.jsonLd.DanubeTechMapper;
 import org.eclipse.tractusx.ssi.lib.util.TestResourceUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,5 +25,28 @@ public class VerifiablePresentationTest {
     Assertions.assertEquals(
         mapFromJson.get(VerifiablePresentation.VERIFIABLE_CREDENTIAL),
         vp.get(VerifiablePresentation.VERIFIABLE_CREDENTIAL));
+  }
+
+  @Test
+  public void shouldNotRemoveContext() {
+    var vpFromMap = TestResourceUtil.getBPNVerifiableCredential();
+    var vc = new VerifiableCredential(vpFromMap);
+    var vpMapped = DanubeTechMapper.map(vc);
+
+    var ctx =
+        URI.create(
+            "https://raw.githubusercontent.com/catenax-ng/product-core-schemas/main/businessPartnerData");
+
+    Assertions.assertTrue(vpMapped.getContexts().contains(ctx));
+  }
+
+  @Test
+  public void shouldLoadCachedContext() {
+    var vpFromMap = TestResourceUtil.getBPNVerifiableCredential();
+    var vc = com.danubetech.verifiablecredentials.VerifiableCredential.fromMap(vpFromMap);
+    Assertions.assertDoesNotThrow(
+        () -> {
+          vc.normalize("urdna2015");
+        });
   }
 }
